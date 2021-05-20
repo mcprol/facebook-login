475054b25521fd8262dfbc722dfb559a1c51fddf
mcprol_metricool <marcoscacabelos@metricool.com>
2021-05-20 09:37:01 +0200

    changes in login to allow 'manage' permissions

--- android/src/main/java/com/getcapacitor/community/facebooklogin/FacebookLogin.java
+++ android/src/main/java/com/getcapacitor/community/facebooklogin/FacebookLogin.java
@@ -180,7 +180,8 @@ public class FacebookLogin extends Plugin {
             return;
         }
 
-        LoginManager.getInstance().logInWithReadPermissions(this.getActivity(), permissions);
+        //LoginManager.getInstance().logInWithReadPermissions(this.getActivity(), permissions);
+        LoginManager.getInstance().logIn(this.getActivity(), permissions);
 
         saveCall(call);
     }
---
+++ dist/esm/web.d.ts
@@ -0,0 +1,20 @@
+import { WebPlugin } from '@capacitor/core';
+import { FacebookLoginPlugin, FacebookLoginResponse, FacebookCurrentAccessTokenResponse } from './definitions';
+declare global {
+    interface Window {
+        fbAsyncInit: Function;
+    }
+}
+export declare class FacebookLoginWeb extends WebPlugin implements FacebookLoginPlugin {
+    constructor();
+    login(options: {
+        permissions: string[];
+    }): Promise<FacebookLoginResponse>;
+    logout(): Promise<void>;
+    getCurrentAccessToken(): Promise<FacebookCurrentAccessTokenResponse>;
+    getProfile<T extends object>(options: {
+        fields: readonly string[];
+    }): Promise<T>;
+}
+declare const FacebookLogin: FacebookLoginWeb;
+export { FacebookLogin };
